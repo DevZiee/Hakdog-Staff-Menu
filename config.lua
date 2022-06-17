5afb683d6045384a0e6ef118dfad137cc91198e3
Collective <86518186+DevZiee@users.noreply.github.com>
2022-06-18 04:19:12 +0800

    Building
    Added Client & Server Files
    Added Config
    Added Fxmanifest

---
+++ config.lua
@@ -0,0 +1,4 @@
+Config = {}
+
+Config.Notification = 't-notify' -- mythic-notify, t-notify, Roda_notification, ESX-Notification
+Config.OneSync = 'infinity' -- infinity or onesync-legacy
---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+games {'gta5'}
+
+author 'Hakdog Utilities'
+description 'Staff Menu'
+version '0.0.1'
+
+shared_scipts{'config.lua'}
+client_scripts{'client/*.lua'}
+server_scripts{'server/*.lua'}
